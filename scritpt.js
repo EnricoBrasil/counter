c7efc05e68f375323e3f8e60164579b2d706761b
enrico brasil <enrico.brasil@prit.app>
2024-09-27 08:58:37 -0300

    add calc

--- scritpt.js
+++ scritpt.js
@@ -1,7 +1,13 @@
-const num = document.getElementById('contador')
-const sub = document.getElementById('dec')
-const add = document.getElementById('add')
+const num = document.getElementById('contador');
+const sub = document.getElementById('dec');
+const add = document.getElementById('add');
+const resul = document.getElementById('resultado');
+const n1 = document.getElementById('m1').value;
+const n2 = document.getElementById('m2').value;
+const n3 = document.getElementById('m3').value;
+const media = document.getElementById('calc');
 
+// contador
 add.addEventListener('click', ()=>{
   num.innerText = parseInt(num.innerText)+1
 })
@@ -9,3 +15,11 @@ add.addEventListener('click', ()=>{
 sub.addEventListener('click', ()=>{
     num.innerText = parseInt(num.innerText)-1
 })
+
+// calcular media
+function calcularMedia(n1,n2,n3)
+{
+   return (n1+n2+n3)/3
+}
+
+let res = calcularMedia(2,2,4)
